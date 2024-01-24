b2db323e3878962c7ced702d2134ea14fb2093c2
Victor Siqueira <victorsiqueira20222@gmail.com>
2024-01-24 19:14:54 -0300

    commit 1

---
+++ entrga-3-pame trabalho final.ts
@@ -0,0 +1,10 @@
+//nota_vendedor é a média das notas que cada usuário irá dar a experiência com o vendedor
+export class Vendedor {
+vendedor_id:number;
+nome:string;
+nota_vendedor:number;
+email:string;
+senha:string;
+telefone:number;
+
+}
